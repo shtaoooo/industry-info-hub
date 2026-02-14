3ed7b36eec7968bd86a797325be68f596d2b8303
shtaoooo <shtaoooo@users.noreply.github.com>
2026-02-14 13:40:46 +0800

    feat: re-add GSI for News and Blogs tables after drift fix

--- cdk/lib/industry-portal-stack.ts
+++ cdk/lib/industry-portal-stack.ts
@@ -106,7 +106,11 @@ export class IndustryPortalStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    // GSI temporarily removed to fix CloudFormation drift - will be re-added
+    newsTable.addGlobalSecondaryIndex({
+      indexName: 'IndustryIndex',
+      partitionKey: { name: 'industryId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'publishedAt', type: dynamodb.AttributeType.STRING },
+    });
 
     const blogsTable = new dynamodb.Table(this, 'BlogsTable', {
       tableName: 'IndustryPortal-Blogs',
@@ -116,6 +120,12 @@ export class IndustryPortalStack extends cdk.Stack {
       pointInTimeRecovery: true,
       encryption: dynamodb.TableEncryption.AWS_MANAGED,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    blogsTable.addGlobalSecondaryIndex({
+      indexName: 'IndustryIndex',
+      partitionKey: { name: 'industryId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'publishedAt', type: dynamodb.AttributeType.STRING },
     });
 
     const accountsTable = new dynamodb.Table(this, 'AccountsTable', {
